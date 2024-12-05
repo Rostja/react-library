90990d035c124c4cb1095f3bfb7f2dc6ac419eca
Rostja <rasto.dorcak@gmail.com>
2024-12-05 17:26:08 +0100

    Changes: App.tsx Adding: Navbar.tsx, ExploreTopBooks.tsx, Carousel.tsx, Return.tsx, Heros.tsx

---
+++ src/layouts/HomePage/Heros.tsx
@@ -0,0 +1,22 @@
+export const Heros = () => {
+    return(
+        <div>
+            <div className="d-none d-lg-block">
+                <div className="row g-0 mt-5">
+                    <div className="col-image-left">
+                    </div>
+                    <div className="col-4 col-md-4 container d-flex justify-content-center align-items-center">
+                        <div className="ml-2">
+                            <h1>What have you been reading?</h1>
+                            <p className="lead">
+                                The library team would love to know what you have been reading. 
+                                Whether it is to learn a skill or grow as a reader, 
+                                we’ll be able to provide recommendations that are relevant to your interests.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
