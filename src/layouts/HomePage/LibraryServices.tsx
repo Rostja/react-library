0b011a58ce78f24a6e9a8036f3c9b3851b17464c
Rostja <rasto.dorcak@gmail.com>
2024-12-06 14:17:56 +0100

    Adding Heros.tsx, LibraryServices.tsx, Footer.tsx Update App.tsx

---
+++ src/layouts/HomePage/LibraryServices.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+export const LibraryServices = () => {
+    return (
+        <div className="container my-5">
+            <div className="row p-4 align-items-center border shadow-lg">
+                <div className="col-lg-7 p-3">
+                    <h1 className="display-4 fw-bold">
+                    Can't find what you are looking for?
+                    </h1>
+                    <p className="lead">
+                        If you can't find what you are looking for,
+                        dont hesitate to contact our library staff.
+                    </p>
+                    <div className="d-grid gap-2 justify-content-md-start mb-4 mb-lg-3">
+                        <a
+                            className="btn main-color btn-lg text-white"
+                            href="#"
+                        >
+                            Sign up
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
