ec50aac1e1ae6107a7590302861616e3f080e9ec
Rostja <rasto.dorcak@gmail.com>
2024-12-12 11:03:29 +0100

    Update index.tsx -> BrowserRouter App.tsx -> Route react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,32 @@ import React from 'react';
 import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
-/* import { HomePage } from './layouts/HomePage/HomePage'; */
+import { HomePage } from './layouts/HomePage/HomePage'; 
 import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
 
 export const App =() => {
   return (
     <div>
     <Navbar/>
-    {/* <HomePage/> */}
+
+    <Switch>
+
+    <Route path='/' exact>
+    <Redirect to='/home' /> 
+    </Route>
+
+    <Route path='/home'>
+    <HomePage/> 
+    </Route>
+    
+    <Route path='/search'>
     <SearchBooksPage/>
+    </Route>
+
+    </Switch>
+
     <Footer/>
     </div>
   );
