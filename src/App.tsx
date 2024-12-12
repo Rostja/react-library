7e8ae5305b3e1060ebdd9edf56d48d22d4b5f9c1
Rostja <rasto.dorcak@gmail.com>
2024-12-12 15:01:43 +0100

    Footer on bottom Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,33 @@
-import React from 'react';
-import './App.css';
-import { Navbar } from './layouts/NavbarAndFooter/Navbar';
-import { Footer } from './layouts/NavbarAndFooter/Footer';
-import { HomePage } from './layouts/HomePage/HomePage'; 
-import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
-import { Redirect, Route, Switch } from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
+import { Footer } from "./layouts/NavbarAndFooter/Footer";
+import { HomePage } from "./layouts/HomePage/HomePage";
+import { SearchBooksPage } from "./layouts/SearchBooksPage/SearchBooksPage";
+import { Redirect, Route, Switch } from "react-router-dom";
 
-
-export const App =() => {
+export const App = () => {
   return (
-    <div>
-    <Navbar/>
+    <div className="d-flex flex-column min-vh-100">
+      <Navbar />
+      <div className="flex-grow-1">
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/home" />
+          </Route>
 
-    <Switch>
+          <Route path="/home">
+            <HomePage />
+          </Route>
 
-    <Route path='/' exact>
-    <Redirect to='/home' /> 
-    </Route>
-
-    <Route path='/home'>
-    <HomePage/> 
-    </Route>
-    
-    <Route path='/search'>
-    <SearchBooksPage/>
-    </Route>
-
-    </Switch>
-
-    <Footer/>
+          <Route path="/search">
+            <SearchBooksPage />
+          </Route>
+        </Switch>
+      </div>
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
