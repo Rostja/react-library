90dcb33d09fcdb59e87ecb2b5b8dc6037bd34430
Rostja <rasto.dorcak@gmail.com>
2024-12-17 19:22:18 +0100

    New OktaSignInWidget.jsx, Update LoginWidget.jsx

--- src/Auth/LoginWidget.jsx
+++ src/Auth/LoginWidget.jsx
@@ -2,12 +2,13 @@
 import { Redirect } from 'react-router-dom';
 import { useOktaAuth } from '@okta/okta-react';
 import { SpinnerLoading } from '../layouts/Utils/SpinnerLoading';
+import OktaSignInWidget from './OktaSignInWidget';
 
 const LoginWidget = ({ config }) => {
     const { oktaAuth,authState} = useOktaAuth();
     const onSuccess = (tokens) => {
         oktaAuth.handleLoginRedirect(tokens);
-    }
+    };
     const onError = (err) => {
         console.log('Sign in error: ', err);
 
@@ -20,7 +21,7 @@ const LoginWidget = ({ config }) => {
     return authState.isAutethenticated ?
     <Redirect to = {{ pathname: '/'}}/>
     :
-    <div></div>
+    <OktaSignInWidget config={config} onSuccess={onSuccess} onError={onError}/>
 };
 
 export default LoginWidget;
