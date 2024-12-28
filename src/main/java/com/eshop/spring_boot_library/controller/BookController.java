8a19b87ad593dcfb830ef08ce3ce53e06564aa41
Rostja <rasto.dorcak@gmail.com>
2024-12-28 17:22:57 +0100

    Update application.properties -> okta.oauth2 BookController.java ExtractJWT.java

--- src/main/java/com/eshop/spring_boot_library/controller/BookController.java
+++ src/main/java/com/eshop/spring_boot_library/controller/BookController.java
@@ -2,6 +2,7 @@ package com.eshop.spring_boot_library.controller;
 
 import com.eshop.spring_boot_library.entity.Book;
 import com.eshop.spring_boot_library.service.BookService;
+import com.eshop.spring_boot_library.utils.ExtractJWT;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,19 +18,21 @@ public class BookController {
     }
 
     @GetMapping("/secure/currentloans/count")
-    public int currentLoansCount(){
+    public int currentLoansCount(@RequestHeader(value = "Authorization") String token){
         String userEmail = "testuser@email.com";
         return bookService.currentLoansCount(userEmail);
     }
 
     @GetMapping("/secure/ischeckedout/byuser")
-    public Boolean checkoutBookByUser(@RequestParam Long bookId){
-        String userEmail = "testuser@email.com";
+    public Boolean checkoutBookByUser(@RequestHeader(value = "Authorization") String token,
+                                      @RequestParam Long bookId){
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
         return bookService.checkoutBookByUser(userEmail, bookId);
     }
 
     @PutMapping("/secure/checkout")
-    public Book checkoutBook (@RequestParam Long bookId) throws Exception {
+    public Book checkoutBook (@RequestHeader(value = "Authorization") String token,
+                              @RequestParam Long bookId) throws Exception {
         String userEmail = "testuser@email.com";
         return bookService.checkoutBook(userEmail, bookId);
     }
