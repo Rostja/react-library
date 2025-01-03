b40c89e8791d886e883731e7adf93ddeb06d8756
Rostja <rasto.dorcak@gmail.com>
2025-01-03 15:48:30 +0100

    Update BookCheckoutPage.tsx, CheckoutAndReviewBox.tsx, LeaveA Review.tsx

--- src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -270,7 +270,7 @@ export const BookCheckoutPage = () => {
                     </div>
                     <CheckoutAndReviewBox book={book} mobile={false} currentLoansCount={currentLoansCount}
                     isAuthenticated={authState?.isAuthenticated} isCheckedOut={isBookCheckedOut}
-                    checkoutBook={checkoutBook} isReviewLeft={isReviewLeft}/>
+                    checkoutBook={checkoutBook} isReviewLeft={isReviewLeft} submitReview={submitReview}/>
                 </div>
                 <hr/>
                 <LatestReviews reviews={reviews} bookId={book?.id} mobile={false} />
@@ -294,7 +294,7 @@ export const BookCheckoutPage = () => {
                 </div>
                 <CheckoutAndReviewBox book={book} mobile={true} currentLoansCount={currentLoansCount}
                  isAuthenticated={authState?.isAuthenticated} isCheckedOut={isBookCheckedOut}
-                 checkoutBook={checkoutBook} isReviewLeft={isReviewLeft}/>
+                 checkoutBook={checkoutBook} isReviewLeft={isReviewLeft} submitReview={submitReview}/>
                 <hr/>
                 <LatestReviews reviews={reviews} bookId={book?.id} mobile={true} />
             </div>
