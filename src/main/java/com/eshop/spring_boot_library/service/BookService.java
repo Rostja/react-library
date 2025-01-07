28443a9b3e93f5e97672790a23d128ed10350daa
Rostja <rasto.dorcak@gmail.com>
2025-01-07 13:38:18 +0100

    Update BookService.java

--- src/main/java/com/eshop/spring_boot_library/service/BookService.java
+++ src/main/java/com/eshop/spring_boot_library/service/BookService.java
@@ -106,4 +106,22 @@ public class BookService {
         bookRepository.save(book.get());
         checkoutRepository.deleteById(validateCheckout.getId());
     }
+
+    public void renewLoan(String userEmail, Long bookId) throws Exception {
+        Checkout validateCheckout = checkoutRepository.findByUserEmailAndBookId(userEmail, bookId);
+
+        if (validateCheckout == null) {
+            throw new Exception("Book does not exist or not checked out by user");
+        }
+
+        SimpleDateFormat sdfFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+        Date d1 = sdfFormat.parse(validateCheckout.getReturnDate());
+        Date d2 = sdfFormat.parse(LocalDate.now().toString());
+
+        if (d1.compareTo(d2) > 0 || d1.compareTo(d2) == 0) {
+            validateCheckout.setReturnDate(LocalDate.now().plusDays(7).toString());
+            checkoutRepository.save(validateCheckout);
+        }
+    }
 }
