3758175e535b36b3a982fc1fbd618dc51b515575
Rostja <rasto.dorcak@gmail.com>
2025-01-11 17:11:32 +0100

    Update MessagesService.java New AdminQuestionRequest.java

---
+++ src/main/java/com/eshop/spring_boot_library/requestmodels/AdminQuestionRequest.java
@@ -0,0 +1,11 @@
+package com.eshop.spring_boot_library.requestmodels;
+
+import lombok.Data;
+
+@Data
+public class AdminQuestionRequest {
+
+    private Long id;
+
+    private String response;
+}
--- src/main/java/com/eshop/spring_boot_library/service/MessagesService.java
+++ src/main/java/com/eshop/spring_boot_library/service/MessagesService.java
@@ -2,9 +2,12 @@ package com.eshop.spring_boot_library.service;
 
 import com.eshop.spring_boot_library.dao.MessageRepository;
 import com.eshop.spring_boot_library.entity.Message;
+import com.eshop.spring_boot_library.requestmodels.AdminQuestionRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -21,5 +24,17 @@ public class MessagesService {
         Message message = new Message(messageRequest.getTitle(), messageRequest.getQuestion());
         message.setUserEmail(userEmail);
         messageRepository.save(message);
+    }
+
+    public void putMessage(AdminQuestionRequest adminQuestionRequest, String userEmail) throws Exception {
+        Optional<Message> message = messageRepository.findById(adminQuestionRequest.getId());
+        if (!message.isPresent()) {
+            throw new Exception("Message does not exist");
+        }
+
+        message.get().setAdminEmail(userEmail);
+        message.get().setResponse(adminQuestionRequest.getResponse());
+        message.get().setClosed(true);
+        messageRepository.save(message.get());
     }
 }
