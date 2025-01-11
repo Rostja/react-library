978240a356249763f2824ab44805e355906c75f2
Rostja <rasto.dorcak@gmail.com>
2025-01-11 18:24:25 +0100

    Update MessagesController.java

--- src/main/java/com/eshop/spring_boot_library/controller/MessagesController.java
+++ src/main/java/com/eshop/spring_boot_library/controller/MessagesController.java
@@ -1,6 +1,7 @@
 package com.eshop.spring_boot_library.controller;
 
 import com.eshop.spring_boot_library.entity.Message;
+import com.eshop.spring_boot_library.requestmodels.AdminQuestionRequest;
 import com.eshop.spring_boot_library.service.MessagesService;
 import com.eshop.spring_boot_library.utils.ExtractJWT;
 import org.springframework.web.bind.annotation.*;
@@ -21,5 +22,17 @@ public class MessagesController {
                             @RequestBody Message messageRequest) {
         String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
         messagesService.postMessage(messageRequest, userEmail);
+    }
+
+    @PutMapping("/secure/admin/message")
+    public void putMessage(@RequestHeader(value="Authorization") String token,
+                           @RequestBody AdminQuestionRequest adminQuestionRequest) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if(admin == null || !admin.equals("admin")) {
+            throw new Exception("Administration page only.");
+        }
+        messagesService.putMessage(adminQuestionRequest, userEmail);
+
     }
 }
