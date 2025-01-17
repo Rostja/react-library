99925c08f06fb67b8cd125dbc70c0641d6ac0c9c
Rostja <rasto.dorcak@gmail.com>
2025-01-17 16:41:01 +0100

    New .env, localhost.conf, localhost.crt, localhost.key Update oktaConfig.json, package.json

--- src/lib/oktaConfig.ts
+++ src/lib/oktaConfig.ts
@@ -1,7 +1,7 @@
 export const oktaConfig = {
     clientId: '0oam5nhgayhFJUi5M5d7' ,
     issuer:'https://dev-54604687.okta.com/oauth2/default',
-    redirectUrl: 'http://localhost:3000/login/callback',
+    redirectUrl: 'https://localhost:3000/login/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
     disableHttpsCheck: true,  // scopes requested by your app
