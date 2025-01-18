0a74e1a446ff37830562ec4a9e191d2d420a6e49
Rostja <rasto.dorcak@gmail.com>
2025-01-18 22:26:45 +0100

    Update index.html, index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+
+const stripePromise = loadStripe('pk_test_51QiKdoCIQ3QGxpKn0UMbkZHNnrRPFZlbLm6ceATeLRB7EFCoDt70BaJFBsXhikdjbpd7iUSZ7D38W9hH25aAKC4S00rdEQh2up');
 
 
 const root = ReactDOM.createRoot(
@@ -11,7 +15,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <BrowserRouter>
-    <App />
+  <Elements stripe={stripePromise}>
+     <App />
+  </Elements>
   </BrowserRouter>
 );
 
