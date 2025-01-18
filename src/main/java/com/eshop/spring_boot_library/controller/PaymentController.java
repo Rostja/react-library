9db5174190cb6a00602253f8baf9eff4862ff76a
Rostja <rasto.dorcak@gmail.com>
2025-01-18 11:23:18 +0100

    New PaymentController.java

---
+++ src/main/java/com/eshop/spring_boot_library/controller/PaymentController.java
@@ -0,0 +1,20 @@
+package com.eshop.spring_boot_library.controller;
+
+import com.eshop.spring_boot_library.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("https://localhost:3000")
+@RestController
+@RequestMapping("/api/payment/secure")
+public class PaymentController {
+
+    private PaymentService paymentService;
+
+    @Autowired
+    public PaymentController(PaymentService paymentService){
+        this.paymentService = paymentService;
+    }
+}
