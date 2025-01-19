a3563a892e41eb2243a9e711c100d22698782ee8
Rostja <rasto.dorcak@gmail.com>
2025-01-19 17:39:18 +0100

    Update Navbar.tsx, PaymentPage.tsx

--- src/layouts/NavbarAndFooter/Navbar.tsx
+++ src/layouts/NavbarAndFooter/Navbar.tsx
@@ -15,7 +15,6 @@ export const Navbar = () => {
 
   const handleLogout = async () => oktaAuth.signOut();
 
-  console.log(authState);
 
   return (
     <nav className="navbar navbar-expand-lg navbar-dark main-color py-3">
@@ -48,6 +47,11 @@ export const Navbar = () => {
             {authState.isAuthenticated &&
               <li className="nav-item">                                                                                                                  
                 <NavLink className="nav-link" to="/shelf">Shelf</NavLink>
+              </li>
+              } 
+              {authState.isAuthenticated &&
+              <li className="nav-item">                                                                                                                  
+                <NavLink className="nav-link" to="/fees">Pay fees</NavLink>
               </li>
               } 
               {authState.isAuthenticated && authState.accessToken?.claims?.userType === 'admin' &&
--- src/layouts/PaymentPage/PaymentPage.tsx
+++ src/layouts/PaymentPage/PaymentPage.tsx
@@ -1,30 +1,30 @@
-import {useOktaAuth} from '@okta/okta-react';
-import {useEffect, useState} from 'react';
-import { SpinnerLoading } from '../Utils/SpinnerLoading';
-import { CardElement } from '@stripe/react-stripe-js';
+import { useOktaAuth } from '@okta/okta-react';
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import PaymentInfoRequest from '../../models/PaymentInfoRequest';
+import { SpinnerLoading } from '../Utils/SpinnerLoading';
+import { StripeCardElement } from '@stripe/stripe-js/types/stripe-js/elements/card';
 
 export const PaymentPage = () => {
-
-    const{authState} = useOktaAuth();
-    const [httpError,setHttpError] = useState(false);
+    
+    const {authState} = useOktaAuth();
+    const [httpError, setHttpError] = useState(false);
     const [submitDisabled, setSubmitDisabled] = useState(false);
     const [fees, setFees] = useState(0);
     const [loadingFees, setLoadingFees] = useState(true);
 
     useEffect(() => {
-        const fetchFees = async() => {
-            if(authState && authState.isAuthenticated){
+        const fetchFees = async () => {
+            if (authState && authState.isAuthenticated) {
                 const url = `${process.env.REACT_APP_API}/payments/search/findByUserEmail?userEmail=${authState.accessToken?.claims.sub}`;
                 const requestOptions = {
                     method: 'GET',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
+                    headers: { 'Content-Type': 'application/json' }
                 };
                 const paymentResponse = await fetch(url, requestOptions);
-                if(!paymentResponse.ok){
-                    throw new Error('Something went wrong!');
+                if (!paymentResponse.ok) {
+                    throw new Error('Something went wrong!')
                 }
                 const paymentResponseJson = await paymentResponse.json();
                 setFees(paymentResponseJson.amount);
@@ -37,6 +37,71 @@ export const PaymentPage = () => {
         })
     }, [authState]);
 
+    const elements = useElements();
+    const stripe = useStripe();
+
+    async function checkout() {
+        if (!stripe || !elements || !elements.getElement(CardElement)) {
+            return;
+        }
+
+        setSubmitDisabled(true);
+
+        let paymentInfo = new PaymentInfoRequest(Math.round(fees * 100), 'USD', authState?.accessToken?.claims.sub);
+
+        const url = `https://localhost:8443/api/payment/secure/payment-intent`;
+        const requestOptions = {
+            method: 'POST',
+            headers: {
+                Authorization: `Bearer ${authState?.accessToken?.accessToken}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(paymentInfo)
+        };
+        const stripeResponse = await fetch(url, requestOptions);
+        if (!stripeResponse.ok) {
+            setHttpError(true);
+            setSubmitDisabled(false);
+            throw new Error('Something went wrong!');
+        }
+        const stripeResponseJson = await stripeResponse.json();
+
+
+        stripe.confirmCardPayment(
+            stripeResponseJson.client_secret, {
+                payment_method: {
+                    card: elements.getElement(CardElement)! as unknown as StripeCardElement,
+                    billing_details: {
+                        email: authState?.accessToken?.claims.sub
+                    }
+                }
+            }, {handleActions: false}
+        ).then(async function (result: any) {
+            if (result.error) {
+                setSubmitDisabled(false)
+                alert('There was an error')
+            } else {
+                const url = `https://localhost:8443/api/payment/secure/payment-complete`;
+                const requestOptions = {
+                    method: 'PUT',
+                    headers: {
+                        Authorization: `Bearer ${authState?.accessToken?.accessToken}`,
+                        'Content-Type': 'application/json'
+                    }
+                };
+                const stripeResponse = await fetch(url, requestOptions);
+                if (!stripeResponse.ok) {
+                    setHttpError(true)
+                    setSubmitDisabled(false)
+                    throw new Error('Something went wrong!')
+                }
+                setFees(0);
+                setSubmitDisabled(false);
+            }
+        });
+        setHttpError(false);
+    }
+
     if (loadingFees) {
         return (
             <SpinnerLoading/>
@@ -44,12 +109,13 @@ export const PaymentPage = () => {
     }
 
     if (httpError) {
-       return (
-        <div className='container m-5'>
-            <p>{httpError}</p>
-        </div>
+        return (
+            <div className='container m-5'>
+                <p>{httpError}</p>
+            </div>
         )
     }
+
 
     return(
         <div className='container'>
@@ -58,22 +124,22 @@ export const PaymentPage = () => {
                 <div className='card-body'>
                     <h5 className='card-title mb-3'>Credit Card</h5>
                     <CardElement id='card-element' />
-                    <button disabled={submitDisabled} type='button' className='btn btm-md main-color text-white mt-3'>
+                    <button disabled={submitDisabled} type='button' className='btn btn-md main-color text-white mt-3' 
+                        onClick={checkout}>
                         Pay fees
                     </button>
                 </div>
-            </div>
-            }
+            </div>}
 
-            {fees === 0 &&
-            <div className='mt-3'>
-                <h5>You have no fees!</h5>
-                <Link type='button' className='btn main-color text-white' to='search'>
-                    Explore top books
-                </Link>
-            </div>
-                }
-                {submitDisabled && <SpinnerLoading/>}
+            {fees === 0 && 
+                <div className='mt-3'>
+                    <h5>You have no fees!</h5>
+                    <Link type='button' className='btn main-color text-white' to='search'>
+                        Explore top books
+                    </Link>
+                </div>
+            }
+            {submitDisabled && <SpinnerLoading/>}
         </div>
     );
 }
